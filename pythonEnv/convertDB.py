ad3b79c241ac1169dcba0232286e8c09ffe44f9c
pedrofmu <pedrofm@pedrofm.dev>
2024-04-08 19:00:15 +0200

    cositas

--- pythonEnv/convertDB.py
+++ pythonEnv/convertDB.py
@@ -1,23 +1,24 @@
 import sqlite3
 
 # Conexión a la base de datos anterior
-old_db_conn = sqlite3.connect('ruta_a_tu_base_de_datos_anterior.db')
+old_db_conn = sqlite3.connect('ingresosVieja.db')
 old_cursor = old_db_conn.cursor()
 
 # Conexión a la nueva base de datos
-new_db_conn = sqlite3.connect('ruta_a_tu_nueva_base_de_datos.db')
+new_db_conn = sqlite3.connect('ingresos.db')
 new_cursor = new_db_conn.cursor()
 
 # Migrar datos de la tabla facturas
 old_cursor.execute('SELECT * FROM facturas')
 facturas = old_cursor.fetchall()
 for factura in facturas:
+    print(factura)
     # Obtener los valores de la factura
-    numero, receptor, emisor, fechaEmision, unidades, concepto, importeTotal, irpf, detalles = factura
+    numero, receptor, emisor, fecha, unidades, concepto, importeTotal, irpf, detalles, formaDePago = factura
 
     # Insertar los datos en la nueva tabla facturas
-    new_cursor.execute('INSERT INTO facturas (numero, receptor, emisor, fechaEmision, unidades, concepto, importeTotal, irpf, detalles, formaDePago) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
-                       (numero, receptor, emisor, fechaEmision, unidades, concepto, importeTotal, irpf, detalles, ''))
+    new_cursor.execute('INSERT INTO facturas (numero, receptor, emisor, fechaEmision, fechaVencimiento, unidades, concepto, importeTotal, irpf, detalles, formaDePago) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
+                       (numero, receptor, emisor, fecha, 'PENDIENTE', unidades, concepto, importeTotal, irpf, detalles, formaDePago))
 
 # Migrar datos de la tabla receptor
 old_cursor.execute('SELECT * FROM receptor')
